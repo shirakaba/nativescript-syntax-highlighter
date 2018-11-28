19ce77ffebb4d512af93bf9ef9937d96c766e44b
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-11-28 10:08:38 +0000

    module typings

--- src/typings/objc!Highlightr.d.ts
+++ src/typings/objc!Highlightr.d.ts
@@ -4,6 +4,10 @@ declare class Highlightr extends NSObject {
 
 	static new(): Highlightr; // inherited from NSObject
 
+	constructor(o: { highlightPath: string; });
+
+	availableThemes(): NSArray<string>;
+
 	// constructor(o: { });
 	// init(): this;
 	initWithHighlightPath(path?: string|null): this;
@@ -12,3 +16,39 @@ declare class Highlightr extends NSObject {
 
 	highlightAsFastRender(code: string, lang: string|null, fastRender: boolean): NSAttributedString;
 }
+
+declare var HighlightrVersionNumber: number;
+
+declare var HighlightrVersionString: interop.Reference<number>;
+
+declare class CodeAttributedString extends NSTextStorage {
+
+	static alloc(): CodeAttributedString; // inherited from NSObject
+
+    static new(): CodeAttributedString; // inherited from NSObject
+    
+    highlightDelegate: HighlightDelegate;
+    
+    readonly highlightr: Highlightr;
+
+    /* Should exist, but doesn't seem to work. */
+	// constructor(o: { highlightr?: Highlightr|null });
+    // initWithHighlightr(highlightr?: Highlightr|null): this;
+    
+    constructor();
+    init(): this;
+    
+    language: string|null;
+
+    highlightRange(range: NSRange): void;
+
+    setupListeners(): void;
+}
+
+interface HighlightDelegate extends NSObjectProtocol {
+    shouldHighlightRange?(range: NSRange): boolean;
+    didHighlightRange?(range: NSRange): void;
+}
+declare var HighlightDelegate: {
+	prototype: HighlightDelegate;
+};
