447ddc6a16e86f5b9a0e10981d95e60fea20c711
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-12-01 12:13:58 +0000

    Undeclaring crashy ones.

--- src/typings/objc!Highlightr.d.ts
+++ src/typings/objc!Highlightr.d.ts
@@ -1,18 +1,26 @@
+/* Can't find a way to access this as a property on Highlightr without a crash, so won't declare it. */
+// declare class Theme extends NSObject {
+//     themeBackgroundColor: UIColor|null;
+// }
+
 declare class Highlightr extends NSObject {
 
 	static alloc(): Highlightr; // inherited from NSObject
 
 	static new(): Highlightr; // inherited from NSObject
 
-	constructor(o: { highlightPath: string; });
+    constructor(o: { highlightPath: string; });
+    
+    /* All of these crash NativeScript without any error explained, so best not to declare them at all. */
+    // getThemeBackgroundColour(): UIColor|null;
+    // associatedTextView: UITextView|null;
+    // theme: Theme|null;
 
 	availableThemes(): NSArray<string>;
 
 	// constructor(o: { });
 	// init(): this;
     initWithHighlightPath(path?: string|null): this;
-    
-    getThemeBackgroundColour(): UIColor|null;
 
 	setThemeTo(name: string): boolean;
 
