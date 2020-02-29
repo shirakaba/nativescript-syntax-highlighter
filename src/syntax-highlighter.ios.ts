ea88e057a404042a0f74dfa97228e769ccf64268
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-02-29 12:06:11 +0000

    Refactor createNativeView()

--- src/syntax-highlighter.ios.ts
+++ src/syntax-highlighter.ios.ts
@@ -7,7 +7,7 @@ global.moduleMerge(base, exports);
 export class SyntaxHighlighterView extends SyntaxHighlighterViewBase {
     private _textView: UITextView;
     private _highlightr: Highlightr;
-    private _codeAttributedString: CodeAttributedString;
+    private _codeAttributedString: CodeAttributedString; // AKA textStorage
     public code: string;
     public languageName: string | null;
     private _layoutManager: NSLayoutManager;
@@ -15,19 +15,49 @@ export class SyntaxHighlighterView extends SyntaxHighlighterViewBase {
     public theme: SyntaxHighlighterTheme;
 
     createNativeView(): Object {
-        this._codeAttributedString = CodeAttributedString.new();
-        const nativeView = UIView.new();
-        this._layoutManager = NSLayoutManager.new();
+        /* 
+         * You can initialise a CodeAttributedString and take its internal Highlightr instance, or pass
+         * one in explicitly. We'll do the latter, so that it becomes easier to support highlightPath.
+         *
+         * For now, we use the default highlightPath: bundle.path(forResource: "highlight.min", ofType: "js")
+         * */
+        this._highlightr = Highlightr.alloc().initWithHighlightPath(null);
+        this._codeAttributedString = CodeAttributedString.alloc().initWithHighlightr(this._highlightr);
+
+        /**
+         * Apart from defaulting to javascript, the same configuration applied in the example app for Highlightr:
+         * 
+         * @see: https://github.com/raspu/Highlightr/blob/master/Example/Highlightr/SampleCode.swift#L53
+         **/
+        this._codeAttributedString.language = "javascript".toLowerCase();
+        this._layoutManager = NSLayoutManager.alloc().init();
         this._codeAttributedString.addLayoutManager(this._layoutManager);
-        this._textContainer = NSTextContainer.new();
+
+        this._textContainer = NSTextContainer.alloc().initWithSize(CGRectZero.size);
         this._layoutManager.addTextContainer(this._textContainer);
+    
         this._textView = UITextView.alloc().initWithFrameTextContainer(CGRectZero, this._textContainer);
         this._textView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
         this._textView.autocorrectionType = UITextAutocorrectionType.No;
         this._textView.autocapitalizationType = UITextAutocapitalizationType.None;
         this._textView.textColor = UIColor.alloc().initWithWhiteAlpha(0.8, 1.0);
+
+        /**
+         * In the demo, Pojoaque was set as the initial theme, so I'll make that the explicit default.
+         * I think I deduced 25/255 gray to be the default BG colour used for Pojoaque in the demo.
+         * 
+         * @see: https://github.com/raspu/Highlightr/blob/master/Example/Highlightr/SampleCode.swift#L155
+         */
+        this._codeAttributedString.highlightr.setThemeTo("pojoaque"); // Just making the default explicit.
+        this._textView.backgroundColor = UIColor.alloc().initWithRedGreenBlueAlpha(25/255, 25/255, 25/255, 1.0);
+        
+        /**
+         * TODO: extend TextViewBase (not TextView, as we need an NSTextContainer) instead of View to support
+         * all TextView properties.
+         */
+        const nativeView = UIView.new();
         nativeView.addSubview(this._textView);
-        this._highlightr = this._codeAttributedString.highlightr;
+        
         return nativeView;
     }
 
